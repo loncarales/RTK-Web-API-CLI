3639b5bfc800dff8e5e14771ed8d93882133b361
Ales Loncar <ales.loncar@gmail.com>
2022-12-09 18:12:44 +0100

    add raidclient simple ws connect example

---
+++ raidclient.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import asyncio
+from raidtoolkit import RaidToolkitClient
+
+
+async def main():
+    client = RaidToolkitClient()
+    client.connect()
+    accounts = await client.AccountApi.get_accounts()
+    print(accounts)
+    account = await client.AccountApi.get_account_dump(accounts[0]["id"])
+    print(account)
+    client.close()
+
+if __name__ == "__main__":
+    asyncio.run(main())
