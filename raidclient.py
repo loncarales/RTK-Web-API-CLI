6888c8405fcf9e68c059812a474c095fafaeb49b
Ales Loncar <ales.loncar@gmail.com>
2022-12-17 21:30:11 +0100

    try dumping artifacts to JSON file

--- raidclient.py
+++ raidclient.py
@@ -2,17 +2,22 @@
 
 import asyncio
 from raidtoolkit import RaidToolkitClient
+import json
 
 
 async def main():
     client = RaidToolkitClient()
     client.connect()
     accounts = await client.AccountApi.get_accounts()
-    # print(accounts)
-    # account = await client.AccountApi.get_account_dump(accounts[0]["id"])
-    # print(account)
+    print(accounts)
+    account = await client.AccountApi.get_account_dump(accounts[0]["id"])
+    print(account)
     artifacts = await client.AccountApi.get_artifacts(accounts[0]["id"])
-    print(artifacts)
+    # Serializing json
+    json_artifacts = json.dumps(artifacts, indent=4)
+    # Writing to data.json
+    with open("artifacts.json", "w") as outfile:
+        outfile.write(json_artifacts)
     client.close()
 
 if __name__ == "__main__":
