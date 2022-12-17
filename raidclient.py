785662fa3dcbba232bdd6cffeb4e19a2a3d5ef62
Ales Loncar <ales.loncar@gmail.com>
2022-12-17 21:41:29 +0100

    Dump whole account data

--- raidclient.py
+++ raidclient.py
@@ -9,15 +9,12 @@ async def main():
     client = RaidToolkitClient()
     client.connect()
     accounts = await client.AccountApi.get_accounts()
-    print(accounts)
     account = await client.AccountApi.get_account_dump(accounts[0]["id"])
-    print(account)
-    artifacts = await client.AccountApi.get_artifacts(accounts[0]["id"])
     # Serializing json
-    json_artifacts = json.dumps(artifacts, indent=4)
+    json_account = json.dumps(account, indent=4)
     # Writing to data.json
-    with open("artifacts.json", "w") as outfile:
-        outfile.write(json_artifacts)
+    with open("account.json", "w") as outfile:
+        outfile.write(json_account)
     client.close()
 
 if __name__ == "__main__":
