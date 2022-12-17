07748fec85a9e8efeddfde72bcccd147db0acbb7
Ales Loncar <ales.loncar@gmail.com>
2022-12-17 21:03:43 +0100

    test get all artifacts

--- raidclient.py
+++ raidclient.py
@@ -8,9 +8,11 @@ async def main():
     client = RaidToolkitClient()
     client.connect()
     accounts = await client.AccountApi.get_accounts()
-    print(accounts)
-    account = await client.AccountApi.get_account_dump(accounts[0]["id"])
-    print(account)
+    # print(accounts)
+    # account = await client.AccountApi.get_account_dump(accounts[0]["id"])
+    # print(account)
+    artifacts = await client.AccountApi.get_artifacts(accounts[0]["id"])
+    print(artifacts)
     client.close()
 
 if __name__ == "__main__":
