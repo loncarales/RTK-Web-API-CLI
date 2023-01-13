8c024ce27b86a69e2da82c9a36e9637e34a82475
Ales Loncar <ales.loncar@gmail.com>
2023-01-13 18:23:03 +0100

    refactor: rename account-dump to data-dump

--- data-dump.py
+++ data-dump.py
@@ -9,12 +9,19 @@ async def main():
     client = RaidToolkitClient()
     client.connect()
     accounts = await client.AccountApi.get_accounts()
+
     account = await client.AccountApi.get_account_dump(accounts[0]["id"])
     # Serializing json
     json_account = json.dumps(account, indent=4)
-    # Writing to data.json
     with open("account.json", "w") as outfile:
         outfile.write(json_account)
+
+    artifacts = await client.AccountApi.get_artifacts(accounts[0]["id"])
+    # Serializing json
+    json_artifacts = json.dumps(artifacts, indent=4)
+    with open("artifacts.json", "w") as outfile:
+        outfile.write(json_artifacts)
+
     client.close()
 
 if __name__ == "__main__":
